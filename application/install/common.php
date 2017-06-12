d66f679571f5b266c7a9b2963e2e467acc03b636
w138789 <294496623@qq.com>
2017-06-12 18:37:36 +0800

    do installation process

--- application/install/common.php
+++ application/install/common.php
@@ -6,39 +6,44 @@ define('IS_WRITE', true);
  * 系统环境检测
  * @return array 系统环境数据
  */
-function check_env(){
+function check_env()
+{
     $items = array(
-        'os'      => array('操作系统', '不限制', '类Unix', PHP_OS, 'success'),
-        'php'     => array('PHP版本', '5.4.0', '5.3+', PHP_VERSION, 'success'),
-        'upload'  => array('附件上传', '不限制', '2M+', '未知', 'success'),
-        'gd'      => array('GD库', '2.0', '2.0+', '未知', 'success'),
-        'disk'    => array('磁盘空间', '5M', '不限制', '未知', 'success'),
+        'os'     => array('操作系统', '不限制', '类Unix', PHP_OS, 'success'),
+        'php'    => array('PHP版本', '5.4.0', '5.3+', PHP_VERSION, 'success'),
+        'upload' => array('附件上传', '不限制', '2M+', '未知', 'success'),
+        'gd'     => array('GD库', '2.0', '2.0+', '未知', 'success'),
+        'disk'   => array('磁盘空间', '5M', '不限制', '未知', 'success'),
     );
 
     //PHP环境检测
-    if($items['php'][3] < $items['php'][1]){
+    if ($items['php'][3] < $items['php'][1])
+    {
         $items['php'][4] = 'error';
         session('error', true);
     }
 
     //附件上传检测
-    if(@ini_get('file_uploads'))
+    if (@ini_get('file_uploads'))
         $items['upload'][3] = ini_get('upload_max_filesize');
 
     //GD库检测
     $tmp = function_exists('gd_info') ? gd_info() : array();
-    if(empty($tmp['GD Version'])){
+    if (empty($tmp['GD Version']))
+    {
         $items['gd'][3] = '未安装';
         $items['gd'][4] = 'error';
         session('error', true);
-    } else {
+    } else
+    {
         $items['gd'][3] = $tmp['GD Version'];
     }
     unset($tmp);
 
     //磁盘空间检测
-    if(function_exists('disk_free_space')) {
-        $items['disk'][3] = floor(disk_free_space(ROOT_PATH) / (1024*1024)).'M';
+    if (function_exists('disk_free_space'))
+    {
+        $items['disk'][3] = floor(disk_free_space(ROOT_PATH) / (1024 * 1024)) . 'M';
     }
 
     return $items;
@@ -48,39 +53,50 @@ function check_env(){
  * 目录，文件读写检测
  * @return array 检测数据
  */
-function check_dirfile(){
+function check_dirfile()
+{
     $items = array(
-        array('dir',  '可写', 'success', 'uploads/attachment/'),
-        array('dir',  '可写', 'success', 'uploads/avatar/'),
-        array('dir',  '可写', 'success', 'uploads/download/'),
-        array('dir',  '可写', 'success', 'uploads/picture/'),
-        array('dir',  '可写', 'success', 'uploads/editor/'),
+        array('dir', '可写', 'success', 'uploads/attachment/'),
+        array('dir', '可写', 'success', 'uploads/avatar/'),
+        array('dir', '可写', 'success', 'uploads/download/'),
+        array('dir', '可写', 'success', 'uploads/picture/'),
+        array('dir', '可写', 'success', 'uploads/editor/'),
 
     );
 
-    foreach ($items as &$val) {
-        $item =	ROOT_PATH . $val[3];
-        if('dir' == $val[0]){
-            if(!is_writable($item)) {
-                if(is_dir($item)) {
+    foreach ($items as &$val)
+    {
+        $item = ROOT_PATH . $val[3];
+        if ('dir' == $val[0])
+        {
+            if (!is_writable($item))
+            {
+                if (is_dir($item))
+                {
                     $val[1] = '可读';
                     $val[2] = 'error';
                     session('error', true);
-                } else {
+                } else
+                {
                     $val[1] = '不存在';
                     $val[2] = 'error';
                     session('error', true);
                 }
             }
-        } else {
-            if(file_exists($item)) {
-                if(!is_writable($item)) {
+        } else
+        {
+            if (file_exists($item))
+            {
+                if (!is_writable($item))
+                {
                     $val[1] = '不可写';
                     $val[2] = 'error';
                     session('error', true);
                 }
-            } else {
-                if(!is_writable(dirname($item))) {
+            } else
+            {
+                if (!is_writable(dirname($item)))
+                {
                     $val[1] = '不存在';
                     $val[2] = 'error';
                     session('error', true);
@@ -91,23 +107,27 @@ function check_dirfile(){
 
     return $items;
 }
+
 /**
  * 函数检测
  * @return array 检测数据
  */
-function check_func(){
+function check_func()
+{
     $items = array(
-        array('pdo','支持','success','类'),
-        array('pdo_mysql','支持','success','模块'),
-        array('file_get_contents', '支持', 'success','函数'),
-        array('mb_strlen',		   '支持', 'success','函数'),
+        array('pdo', '支持', 'success', '类'),
+        array('pdo_mysql', '支持', 'success', '模块'),
+        array('file_get_contents', '支持', 'success', '函数'),
+        array('mb_strlen', '支持', 'success', '函数'),
     );
 
-    foreach ($items as &$val) {
-        if(('类'==$val[3] && !class_exists($val[0]))
-            || ('模块'==$val[3] && !extension_loaded($val[0]))
-            || ('函数'==$val[3] && !function_exists($val[0]))
-        ){
+    foreach ($items as &$val)
+    {
+        if (('类' == $val[3] && !class_exists($val[0]))
+            || ('模块' == $val[3] && !extension_loaded($val[0]))
+            || ('函数' == $val[3] && !function_exists($val[0]))
+        )
+        {
             $val[1] = '不支持';
             $val[2] = 'error';
             session('error', true);
@@ -115,4 +135,11 @@ function check_func(){
     }
 
     return $items;
+}
+
+function create_tables($db, $prefix = '')
+{
+    //读取SQL文件
+    $sql = file_get_contents(ROOT_PATH . 'data/sql.sql');
+    $sql = str_replace("\r", "\n", $sql);
 }
--- application/install/controller/Index.php
+++ application/install/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\install\controller;
 
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
@@ -37,7 +38,8 @@ class Index extends Controller
         $env = check_env();
 
         //目录文件读写检测
-        if (IS_WRITE) {
+        if (IS_WRITE)
+        {
             $dirfile = check_dirfile();
             $this->assign('dirfile', $dirfile);
         }
@@ -51,6 +53,94 @@ class Index extends Controller
         $this->assign('func', $func);
         $this->status['index'] = 'success';
         $this->status['check'] = 'primary';
+        $this->assign('status', $this->status);
+        return $this->fetch();
+    }
+
+    public function config($db = null, $admin = null)
+    {
+        if (request()->isPost())
+        {
+            //检测管理员信息
+            if (!is_array($admin) || empty($admin[0]) || empty($admin[1]) || empty($admin[2]))
+            {
+                return $this->error('请填写管理员信息!');
+            } elseif ($admin[1] != $admin[2])
+            {
+                return $this->error('确认密码和密码不一致');
+            } else
+            {
+                $info = [];
+                list($info['username'], $info['password'], $info['repassword'], $info['email']) = $admin;
+                //缓存管理员信息
+                session('admin_info', $info);
+            }
+
+            //检测数据库配置
+            if (!is_array($db) || empty($db[0]) || empty($db[1]) || empty($db[2]) || empty($db[3]))
+            {
+                return $this->error('请填写完整的数据库配置');
+            } else
+            {
+                $DB = [];
+                list($DB['type'], $DB['hostname'], $DB['database'], $DB['username'], $DB['password'], $DB['hostport'], $DB['prefix']) = $db;
+                //缓存数据库配置
+                session('admin_config', $DB);
+
+                //创建数据库
+                $dbname = $DB['database'];
+                unset($DB['database']);
+                $db  = Db::connect($DB);
+                $sql = "CREATE DATABASE IF NOT EXISTS `{$dbname}` DEFAULT CHARACTER SET utf8";
+                if (!$db->execute($sql))
+                {
+                    return $this->error($db->getError());
+                } else
+                {
+                    return $this->redirect('install/index/sql');
+                }
+
+            }
+            return json_encode($admin);
+        } else
+        {
+            $this->status['index']  = 'success';
+            $this->status['check']  = 'success';
+            $this->status['config'] = 'primary';
+            $this->assign('status', $this->status);
+            return $this->fetch();
+        }
+
+    }
+
+    public function sql()
+    {
+        session('error',false);
+        $this->status['index'] = 'success';
+        $this->status['check'] = 'success';
+        $this->status['config'] = 'success';
+        $this->status['sql'] = 'primary';
+        $this->assign('status', $this->status);
+        echo $this->fetch();
+        if(session('update')){
+            return '升级数据库未做';
+        }else{
+            //连接数据库
+            $dbconfig = session('db_config');
+            $db = Db::connect($dbconfig);
+            //创建数据表
+            create_tables($db,$dbconfig['prefix']);
+        }
+
+    }
+
+    public function complate()
+    {
+        $this->status['index'] = 'success';
+        $this->status['check'] = 'success';
+        $this->status['config'] = 'success';
+        $this->status['sql'] = 'success';
+        $this->status['complete'] = 'success';
         $this->assign('status', $this->status);
         return $this->fetch();
     }
